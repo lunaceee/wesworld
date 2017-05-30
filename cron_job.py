58677808150db3016a4ce5e7dac864856a40c0f9
luna <lunaceee@gmail.com>
2017-05-29 17:01:42 -0700

    added new files

--- cron_job.py
+++ cron_job.py
@@ -1,12 +1,17 @@
 # delete from cache
 # where created_at < now() - interval '3 days';
+import os
 import datetime
 import sqlalchemy
 from sqlalchemy.schema import Table
 from sqlalchemy.schema import MetaData
 
+print 'Running cron job'
+
+
 too_old = datetime.datetime.today() - datetime.timedelta(days=3)
-engine = sqlalchemy.create_engine('postgresql:///wesworld')
+db_url = os.environ.get('DATABASE_URL', 'postgresql:///wesworld')
+engine = sqlalchemy.create_engine(db_url)
 meta = MetaData()
 meta.reflect(bind=engine)
 cache_table = meta.tables['cache']
