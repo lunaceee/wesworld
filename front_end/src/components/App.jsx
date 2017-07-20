2b85e7e34ef3f76101b25f9a38d390921165219f
luna <lunaceee@gmail.com>
2017-07-19 23:27:51 -0700

    successfully set up webpack and babel

--- front_end/src/components/App.jsx
+++ front_end/src/components/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Nav} from "Nav";
+import Nav from "./Nav.jsx";
 import logo from '../static/css/images/binocular.svg';
 import '../App.css';
 
@@ -7,6 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React, lunayu</h2>
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,28 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'front_end/public');
-var APP_DIR = path.resolve(__dirname, 'front_end/src');
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  entry: APP_DIR + '/App.js',
+  entry: APP_DIR + '/index.js',
   output: {
     path: BUILD_DIR,
     filename: 'bundle.js'
-  }
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
 };
 
+console.log(config.entry);
 module.exports = config;
